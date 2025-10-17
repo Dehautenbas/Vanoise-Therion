2cc53ffaa47f4417186af0a8420a9c7f7c8b0290
Dehautenbas <martin.dehautenbas@gmail.com>
2025-10-17 17:20:04 +0200

    màj amont-2

---
+++ thTMPDIR/data.pl
@@ -0,0 +1,1065 @@
+%SCRAP = (
+	# scrap: escalier_SP1
+	aaaaaaa => {
+		 F => "data.1",
+		 B => "data.2",
+		 I => "data.2bg",
+		 C => "data.2clip",
+		 E => "data.3",
+		 X => "data.4",
+		 P => "data.4bbox",
+		 Y => 1,
+		 V => -1,
+		 S => "2326.72 -532.31",
+	},
+	# scrap: escalier_SP1
+	aaaaaab => {
+		 B => "data.2",
+		 I => "data.2bg",
+		 C => "data.2clip",
+		 Y => 2,
+		 V => -1,
+		 S => "2326.72 -532.31",
+	},
+	# scrap: section1
+	aaaaaac => {
+		 Z => 1,
+		 B => "data.5",
+		 I => "data.5bg",
+		 C => "data.5clip",
+		 E => "data.6",
+		 X => "data.7",
+		 P => "data.7bbox",
+		 Y => 1,
+		 V => -1,
+		 S => "2270.03 -463.32",
+	},
+	# scrap: pieuvre_PG-FM_SP3
+	aaaaaad => {
+		 F => "data.8",
+		 B => "data.9",
+		 I => "data.9bg",
+		 C => "data.9clip",
+		 E => "data.10",
+		 X => "data.11",
+		 P => "data.11bbox",
+		 Y => 1,
+		 V => -2,
+		 S => "988.02 -213.09",
+	},
+	# scrap: pieuvre_PG-FM_SP3
+	aaaaaae => {
+		 B => "data.9",
+		 I => "data.9bg",
+		 C => "data.9clip",
+		 Y => 3,
+		 V => -1,
+		 S => "988.02 -213.09",
+	},
+	# scrap: pieuvre_PG-FM_SP2
+	aaaaaaf => {
+		 F => "data.12",
+		 B => "data.13",
+		 I => "data.13bg",
+		 C => "data.13clip",
+		 E => "data.14",
+		 X => "data.15",
+		 P => "data.15bbox",
+		 Y => 1,
+		 V => -2,
+		 S => "998.84 -228.90",
+	},
+	# scrap: pieuvre_PG-FM_SP2
+	aaaaaag => {
+		 B => "data.13",
+		 I => "data.13bg",
+		 C => "data.13clip",
+		 Y => 3,
+		 V => -1,
+		 S => "998.84 -228.90",
+	},
+	# scrap: pieuvre_PG-FM_SP1
+	aaaaaah => {
+		 F => "data.16",
+		 B => "data.17",
+		 I => "data.17bg",
+		 C => "data.17clip",
+		 E => "data.18",
+		 X => "data.19",
+		 P => "data.19bbox",
+		 Y => 1,
+		 V => -2,
+		 S => "1400.21 -384.24",
+	},
+	# scrap: pieuvre_PG-FM_SP1
+	aaaaaai => {
+		 B => "data.17",
+		 I => "data.17bg",
+		 C => "data.17clip",
+		 Y => 3,
+		 V => -1,
+		 S => "1400.21 -384.24",
+	},
+	# scrap: SP-jonction-3
+	aaaaaaj => {
+		 F => "data.20",
+		 B => "data.21",
+		 I => "data.21bg",
+		 C => "data.21clip",
+		 E => "data.22",
+		 X => "data.23",
+		 P => "data.23bbox",
+		 Y => 1,
+		 V => -15,
+		 S => "417.60 -133.31",
+	},
+	# scrap: SP-jonction-3
+	aaaaaba => {
+		 B => "data.21",
+		 I => "data.21bg",
+		 C => "data.21clip",
+		 Y => 4,
+		 V => -1,
+		 S => "417.60 -133.31",
+	},
+	# scrap: SP-jonction-2
+	aaaaabb => {
+		 F => "data.24",
+		 B => "data.25",
+		 I => "data.25bg",
+		 C => "data.25clip",
+		 E => "data.26",
+		 X => "data.27",
+		 P => "data.27bbox",
+		 Y => 1,
+		 V => -14,
+		 S => "477.04 -271.53",
+	},
+	# scrap: SP-jonction-2
+	aaaaabc => {
+		 B => "data.25",
+		 I => "data.25bg",
+		 C => "data.25clip",
+		 Y => 4,
+		 V => -1,
+		 S => "477.04 -271.53",
+	},
+	# scrap: SP-jonction-1
+	aaaaabd => {
+		 F => "data.28",
+		 B => "data.29",
+		 I => "data.29bg",
+		 C => "data.29clip",
+		 E => "data.30",
+		 X => "data.31",
+		 P => "data.31bbox",
+		 Y => 1,
+		 V => -13,
+		 S => "487.23 -421.13",
+	},
+	# scrap: SP-jonction-1
+	aaaaabe => {
+		 B => "data.29",
+		 I => "data.29bg",
+		 C => "data.29clip",
+		 Y => 4,
+		 V => -1,
+		 S => "487.23 -421.13",
+	},
+	# scrap: SP1_Frequence51
+	aaaaabf => {
+		 F => "data.32",
+		 B => "data.33",
+		 I => "data.33bg",
+		 C => "data.33clip",
+		 E => "data.34",
+		 X => "data.35",
+		 P => "data.35bbox",
+		 Y => 1,
+		 V => -12,
+		 S => "307.69 -179.94",
+	},
+	# scrap: SP1_Frequence51
+	aaaaabg => {
+		 B => "data.33",
+		 I => "data.33bg",
+		 C => "data.33clip",
+		 Y => 4,
+		 V => -1,
+		 S => "307.69 -179.94",
+	},
+	# scrap: SP2_Frequence51
+	aaaaabh => {
+		 F => "data.36",
+		 B => "data.37",
+		 I => "data.37bg",
+		 C => "data.37clip",
+		 E => "data.38",
+		 X => "data.39",
+		 P => "data.39bbox",
+		 Y => 1,
+		 V => -11,
+		 S => "670.38 -294.49",
+	},
+	# scrap: SP2_Frequence51
+	aaaaabi => {
+		 B => "data.37",
+		 I => "data.37bg",
+		 C => "data.37clip",
+		 Y => 4,
+		 V => -1,
+		 S => "670.38 -294.49",
+	},
+	# scrap: SP1-puits_remontant
+	aaaaabj => {
+		 F => "data.40",
+		 B => "data.41",
+		 I => "data.41bg",
+		 C => "data.41clip",
+		 E => "data.42",
+		 X => "data.43",
+		 P => "data.43bbox",
+		 Y => 1,
+		 V => -10,
+		 S => "675.67 -291.87",
+	},
+	# scrap: SP1-puits_remontant
+	aaaaaca => {
+		 B => "data.41",
+		 I => "data.41bg",
+		 C => "data.41clip",
+		 Y => 4,
+		 V => -1,
+		 S => "675.67 -291.87",
+	},
+	# scrap: amont23_SP1
+	aaaaacb => {
+		 F => "data.44",
+		 B => "data.45",
+		 I => "data.45bg",
+		 C => "data.45clip",
+		 E => "data.46",
+		 X => "data.47",
+		 P => "data.47bbox",
+		 Y => 1,
+		 V => -9,
+		 S => "1495.40 -450.72",
+	},
+	# scrap: amont23_SP1
+	aaaaacc => {
+		 B => "data.45",
+		 I => "data.45bg",
+		 C => "data.45clip",
+		 Y => 4,
+		 V => -1,
+		 S => "1495.40 -450.72",
+	},
+	# scrap: PR-amont23
+	aaaaacd => {
+		 F => "data.48",
+		 B => "data.49",
+		 I => "data.49bg",
+		 C => "data.49clip",
+		 E => "data.50",
+		 X => "data.51",
+		 P => "data.51bbox",
+		 Y => 1,
+		 V => -8,
+		 S => "931.84 -307.31",
+	},
+	# scrap: PR-amont23
+	aaaaace => {
+		 B => "data.49",
+		 I => "data.49bg",
+		 C => "data.49clip",
+		 Y => 4,
+		 V => -1,
+		 S => "931.84 -307.31",
+	},
+	# scrap: arcel1_S5_P
+	aaaaacf => {
+		 F => "data.52",
+		 B => "data.53",
+		 I => "data.53bg",
+		 C => "data.53clip",
+		 E => "data.54",
+		 X => "data.55",
+		 P => "data.55bbox",
+		 Y => 1,
+		 V => -7,
+		 S => "466.21 -380.74",
+	},
+	# scrap: arcel1_S5_P
+	aaaaacg => {
+		 B => "data.53",
+		 I => "data.53bg",
+		 C => "data.53clip",
+		 Y => 4,
+		 V => -1,
+		 S => "466.21 -380.74",
+	},
+	# scrap: arcel1_S4_P
+	aaaaach => {
+		 F => "data.56",
+		 B => "data.57",
+		 I => "data.57bg",
+		 C => "data.57clip",
+		 E => "data.58",
+		 X => "data.59",
+		 P => "data.59bbox",
+		 Y => 1,
+		 V => -6,
+		 S => "668.82 -607.25",
+	},
+	# scrap: arcel1_S4_P
+	aaaaaci => {
+		 B => "data.57",
+		 I => "data.57bg",
+		 C => "data.57clip",
+		 Y => 4,
+		 V => -1,
+		 S => "668.82 -607.25",
+	},
+	# scrap: arcel1_S3_P
+	aaaaacj => {
+		 F => "data.60",
+		 B => "data.61",
+		 I => "data.61bg",
+		 C => "data.61clip",
+		 E => "data.62",
+		 X => "data.63",
+		 P => "data.63bbox",
+		 Y => 1,
+		 V => -5,
+		 S => "787.69 -847.99",
+	},
+	# scrap: arcel1_S3_P
+	aaaaada => {
+		 B => "data.61",
+		 I => "data.61bg",
+		 C => "data.61clip",
+		 Y => 4,
+		 V => -1,
+		 S => "787.69 -847.99",
+	},
+	# scrap: arcel1_S2_P
+	aaaaadb => {
+		 F => "data.64",
+		 B => "data.65",
+		 I => "data.65bg",
+		 C => "data.65clip",
+		 E => "data.66",
+		 X => "data.67",
+		 P => "data.67bbox",
+		 Y => 1,
+		 V => -4,
+		 S => "875.57 -936.97",
+	},
+	# scrap: arcel1_S2_P
+	aaaaadc => {
+		 B => "data.65",
+		 I => "data.65bg",
+		 C => "data.65clip",
+		 Y => 4,
+		 V => -1,
+		 S => "875.57 -936.97",
+	},
+	# scrap: arcel1_S1_P
+	aaaaadd => {
+		 F => "data.68",
+		 B => "data.69",
+		 I => "data.69bg",
+		 C => "data.69clip",
+		 E => "data.70",
+		 X => "data.71",
+		 P => "data.71bbox",
+		 Y => 1,
+		 V => -3,
+		 S => "942.80 -1052.79",
+	},
+	# scrap: arcel1_S1_P
+	aaaaade => {
+		 B => "data.69",
+		 I => "data.69bg",
+		 C => "data.69clip",
+		 Y => 4,
+		 V => -1,
+		 S => "942.80 -1052.79",
+	},
+	# scrap: tube-de-prechauffage_SP1
+	aaaaadf => {
+		 F => "data.72",
+		 B => "data.73",
+		 I => "data.73bg",
+		 C => "data.73clip",
+		 E => "data.74",
+		 X => "data.75",
+		 P => "data.75bbox",
+		 Y => 1,
+		 V => -16,
+		 S => "2112.06 -744.52",
+	},
+	# scrap: tube-de-prechauffage_SP1
+	aaaaadg => {
+		 B => "data.73",
+		 I => "data.73bg",
+		 C => "data.73clip",
+		 Y => 5,
+		 V => -1,
+		 S => "2112.06 -744.52",
+	},
+	# scrap: tube-de-prechauffage_SP1
+	aaaaadh => {
+		 B => "data.76",
+		 I => "data.76bg",
+		 C => "data.76clip",
+		 E => "data.77",
+		 X => "data.78",
+		 P => "data.78bbox",
+		 Y => 6,
+		 V => -1,
+		 S => "2107.35 -574.51",
+	},
+	# scrap: pts_oublie_SP1
+	aaaaadi => {
+		 F => "data.79",
+		 B => "data.80",
+		 I => "data.80bg",
+		 C => "data.80clip",
+		 E => "data.81",
+		 X => "data.82",
+		 P => "data.82bbox",
+		 Y => 1,
+		 V => -18,
+		 S => "1606.54 -761.77",
+	},
+	# scrap: pts_oublie_SP1
+	aaaaadj => {
+		 B => "data.80",
+		 I => "data.80bg",
+		 C => "data.80clip",
+		 Y => 7,
+		 V => -1,
+		 S => "1606.54 -761.77",
+	},
+	# scrap: diaclase_MK-SP_SP1
+	aaaaaea => {
+		 F => "data.83",
+		 B => "data.84",
+		 I => "data.84bg",
+		 C => "data.84clip",
+		 E => "data.85",
+		 X => "data.86",
+		 P => "data.86bbox",
+		 Y => 1,
+		 V => -19,
+		 S => "4898.15 -1399.48",
+	},
+	# scrap: diaclase_MK-SP_SP1
+	aaaaaeb => {
+		 B => "data.84",
+		 I => "data.84bg",
+		 C => "data.84clip",
+		 Y => 8,
+		 V => -1,
+		 S => "4898.15 -1399.48",
+	},
+	# scrap: SP_amont2024-3_2
+	aaaaaec => {
+		 F => "data.87",
+		 B => "data.88",
+		 I => "data.88bg",
+		 C => "data.88clip",
+		 E => "data.89",
+		 X => "data.90",
+		 P => "data.90bbox",
+		 Y => 1,
+		 V => -27,
+		 S => "7182.35 -1953.89",
+	},
+	# scrap: SP_amont2024-3_2
+	aaaaaed => {
+		 B => "data.88",
+		 I => "data.88bg",
+		 C => "data.88clip",
+		 Y => 9,
+		 V => -1,
+		 S => "7182.35 -1953.89",
+	},
+	# scrap: SP_amont2024-3_1
+	aaaaaee => {
+		 F => "data.91",
+		 B => "data.92",
+		 I => "data.92bg",
+		 C => "data.92clip",
+		 E => "data.93",
+		 X => "data.94",
+		 P => "data.94bbox",
+		 Y => 1,
+		 V => -26,
+		 S => "6661.11 -1430.32",
+	},
+	# scrap: SP_amont2024-3_1
+	aaaaaef => {
+		 B => "data.92",
+		 I => "data.92bg",
+		 C => "data.92clip",
+		 Y => 9,
+		 V => -1,
+		 S => "6661.11 -1430.32",
+	},
+	# scrap: SP_amont2024-2_2
+	aaaaaeg => {
+		 F => "data.95",
+		 B => "data.96",
+		 I => "data.96bg",
+		 C => "data.96clip",
+		 E => "data.97",
+		 X => "data.98",
+		 P => "data.98bbox",
+		 Y => 1,
+		 V => -25,
+		 S => "6041.50 -1033.43",
+	},
+	# scrap: SP_amont2024-2_2
+	aaaaaeh => {
+		 B => "data.96",
+		 I => "data.96bg",
+		 C => "data.96clip",
+		 Y => 9,
+		 V => -1,
+		 S => "6041.50 -1033.43",
+	},
+	# scrap: SP_amont2024-2_1
+	aaaaaei => {
+		 F => "data.99",
+		 B => "data.100",
+		 I => "data.100bg",
+		 C => "data.100clip",
+		 E => "data.101",
+		 X => "data.102",
+		 P => "data.102bbox",
+		 Y => 1,
+		 V => -24,
+		 S => "5460.01 -924.78",
+	},
+	# scrap: SP_amont2024-2_1
+	aaaaaej => {
+		 B => "data.100",
+		 I => "data.100bg",
+		 C => "data.100clip",
+		 Y => 9,
+		 V => -1,
+		 S => "5460.01 -924.78",
+	},
+	# scrap: SP_amont2024-1_4
+	aaaaafa => {
+		 F => "data.103",
+		 B => "data.104",
+		 I => "data.104bg",
+		 C => "data.104clip",
+		 E => "data.105",
+		 X => "data.106",
+		 P => "data.106bbox",
+		 Y => 1,
+		 V => -23,
+		 S => "4268.47 -903.54",
+	},
+	# scrap: SP_amont2024-1_4
+	aaaaafb => {
+		 B => "data.104",
+		 I => "data.104bg",
+		 C => "data.104clip",
+		 Y => 9,
+		 V => -1,
+		 S => "4268.47 -903.54",
+	},
+	# scrap: SP_amont2024-1_3
+	aaaaafc => {
+		 B => "data.107",
+		 I => "data.107bg",
+		 C => "data.107clip",
+		 E => "data.108",
+		 X => "data.109",
+		 P => "data.109bbox",
+		 Y => 1,
+		 V => -22,
+		 S => "3704.41 -617.47",
+	},
+	# scrap: SP_amont2024-1_3
+	aaaaafd => {
+		 B => "data.107",
+		 I => "data.107bg",
+		 C => "data.107clip",
+		 Y => 9,
+		 V => -1,
+		 S => "3704.41 -617.47",
+	},
+	# scrap: SP_amont2024-1_2
+	aaaaafe => {
+		 F => "data.110",
+		 B => "data.111",
+		 I => "data.111bg",
+		 C => "data.111clip",
+		 E => "data.112",
+		 X => "data.113",
+		 P => "data.113bbox",
+		 Y => 1,
+		 V => -21,
+		 S => "3014.87 -521.42",
+	},
+	# scrap: SP_amont2024-1_2
+	aaaaaff => {
+		 B => "data.111",
+		 I => "data.111bg",
+		 C => "data.111clip",
+		 Y => 9,
+		 V => -1,
+		 S => "3014.87 -521.42",
+	},
+	# scrap: SP_amont2024-1_1
+	aaaaafg => {
+		 F => "data.114",
+		 B => "data.115",
+		 I => "data.115bg",
+		 C => "data.115clip",
+		 E => "data.116",
+		 X => "data.117",
+		 P => "data.117bbox",
+		 Y => 1,
+		 V => -20,
+		 S => "2868.11 -501.75",
+	},
+	# scrap: SP_amont2024-1_1
+	aaaaafh => {
+		 B => "data.115",
+		 I => "data.115bg",
+		 C => "data.115clip",
+		 Y => 9,
+		 V => -1,
+		 S => "2868.11 -501.75",
+	},
+	# scrap: SP_pts_actif_2_1
+	aaaaafi => {
+		 F => "data.118",
+		 B => "data.119",
+		 I => "data.119bg",
+		 C => "data.119clip",
+		 E => "data.120",
+		 X => "data.121",
+		 P => "data.121bbox",
+		 Y => 1,
+		 V => -29,
+		 S => "4121.34 -73.30",
+	},
+	# scrap: SP_pts_actif_2_1
+	aaaaafj => {
+		 B => "data.119",
+		 I => "data.119bg",
+		 C => "data.119clip",
+		 Y => 10,
+		 V => -1,
+		 S => "4121.34 -73.30",
+	},
+	# scrap: SP_pts_actif_1_1
+	aaaaaga => {
+		 F => "data.122",
+		 B => "data.123",
+		 I => "data.123bg",
+		 C => "data.123clip",
+		 E => "data.124",
+		 X => "data.125",
+		 P => "data.125bbox",
+		 Y => 1,
+		 V => -28,
+		 S => "4382.24 -529.43",
+	},
+	# scrap: SP_pts_actif_1_1
+	aaaaagb => {
+		 B => "data.123",
+		 I => "data.123bg",
+		 C => "data.123clip",
+		 Y => 10,
+		 V => -1,
+		 S => "4382.24 -529.43",
+	},
+	# scrap: diaclase-boucle_SP1
+	aaaaagc => {
+		 F => "data.126",
+		 B => "data.127",
+		 I => "data.127bg",
+		 C => "data.127clip",
+		 E => "data.128",
+		 X => "data.129",
+		 P => "data.129bbox",
+		 Y => 1,
+		 V => -30,
+		 S => "3587.91 -286.94",
+	},
+	# scrap: diaclase-boucle_SP1
+	aaaaagd => {
+		 B => "data.127",
+		 I => "data.127bg",
+		 C => "data.127clip",
+		 Y => 11,
+		 V => -1,
+		 S => "3587.91 -286.94",
+	},
+	# scrap: SP2_faille-fossile-riviere
+	aaaaage => {
+		 B => "data.130",
+		 I => "data.130bg",
+		 C => "data.130clip",
+		 E => "data.131",
+		 X => "data.132",
+		 P => "data.132bbox",
+		 Y => 1,
+		 V => -32,
+		 S => "7529.53 -2082.97",
+	},
+	# scrap: SP2_faille-fossile-riviere
+	aaaaagf => {
+		 B => "data.130",
+		 I => "data.130bg",
+		 C => "data.130clip",
+		 Y => 12,
+		 V => -1,
+		 S => "7529.53 -2082.97",
+	},
+	# scrap: SP1_faille-fossile-riviere
+	aaaaagg => {
+		 F => "data.133",
+		 B => "data.134",
+		 I => "data.134bg",
+		 C => "data.134clip",
+		 E => "data.135",
+		 X => "data.136",
+		 P => "data.136bbox",
+		 Y => 1,
+		 V => -31,
+		 S => "7832.22 -2177.09",
+	},
+	# scrap: SP1_faille-fossile-riviere
+	aaaaagh => {
+		 B => "data.134",
+		 I => "data.134bg",
+		 C => "data.134clip",
+		 Y => 12,
+		 V => -1,
+		 S => "7832.22 -2177.09",
+	},
+	# scrap: SP2_amont-2025-2
+	aaaaagi => {
+		 B => "data.137",
+		 I => "data.137bg",
+		 C => "data.137clip",
+		 E => "data.138",
+		 X => "data.139",
+		 P => "data.139bbox",
+		 Y => 1,
+		 V => -34,
+		 S => "7722.11 -5597.29",
+	},
+	# scrap: SP2_amont-2025-2
+	aaaaagj => {
+		 B => "data.137",
+		 I => "data.137bg",
+		 C => "data.137clip",
+		 Y => 13,
+		 V => -1,
+		 S => "7722.11 -5597.29",
+	},
+	# scrap: SP1_amont-2025-2
+	aaaaaha => {
+		 B => "data.140",
+		 I => "data.140bg",
+		 C => "data.140clip",
+		 E => "data.141",
+		 X => "data.142",
+		 P => "data.142bbox",
+		 Y => 1,
+		 V => -33,
+		 S => "7920.46 -4083.39",
+	},
+	# scrap: SP1_amont-2025-2
+	aaaaahb => {
+		 B => "data.140",
+		 I => "data.140bg",
+		 C => "data.140clip",
+		 Y => 13,
+		 V => -1,
+		 S => "7920.46 -4083.39",
+	},
+	# scrap: SP5_amont-2025-1
+	aaaaahc => {
+		 F => "data.143",
+		 B => "data.144",
+		 I => "data.144bg",
+		 C => "data.144clip",
+		 E => "data.145",
+		 X => "data.146",
+		 P => "data.146bbox",
+		 Y => 1,
+		 V => -39,
+		 S => "7645.81 -3156.29",
+	},
+	# scrap: SP5_amont-2025-1
+	aaaaahd => {
+		 B => "data.144",
+		 I => "data.144bg",
+		 C => "data.144clip",
+		 Y => 14,
+		 V => -1,
+		 S => "7645.81 -3156.29",
+	},
+	# scrap: SP1_amont-2025-1
+	aaaaahe => {
+		 F => "data.147",
+		 B => "data.148",
+		 I => "data.148bg",
+		 C => "data.148clip",
+		 E => "data.149",
+		 X => "data.150",
+		 P => "data.150bbox",
+		 Y => 1,
+		 V => -38,
+		 S => "7019.18 -2322.37",
+	},
+	# scrap: SP1_amont-2025-1
+	aaaaahf => {
+		 B => "data.148",
+		 I => "data.148bg",
+		 C => "data.148clip",
+		 Y => 14,
+		 V => -1,
+		 S => "7019.18 -2322.37",
+	},
+	# scrap: SP2_amont-2025-1
+	aaaaahg => {
+		 F => "data.151",
+		 B => "data.152",
+		 I => "data.152bg",
+		 C => "data.152clip",
+		 E => "data.153",
+		 X => "data.154",
+		 P => "data.154bbox",
+		 Y => 1,
+		 V => -37,
+		 S => "7476.38 -2459.54",
+	},
+	# scrap: SP2_amont-2025-1
+	aaaaahh => {
+		 B => "data.152",
+		 I => "data.152bg",
+		 C => "data.152clip",
+		 Y => 14,
+		 V => -1,
+		 S => "7476.38 -2459.54",
+	},
+	# scrap: SP3_amont-2025-1
+	aaaaahi => {
+		 F => "data.155",
+		 B => "data.156",
+		 I => "data.156bg",
+		 C => "data.156clip",
+		 E => "data.157",
+		 X => "data.158",
+		 P => "data.158bbox",
+		 Y => 1,
+		 V => -36,
+		 S => "7611.81 -2897.50",
+	},
+	# scrap: SP3_amont-2025-1
+	aaaaahj => {
+		 B => "data.156",
+		 I => "data.156bg",
+		 C => "data.156clip",
+		 Y => 14,
+		 V => -1,
+		 S => "7611.81 -2897.50",
+	},
+	# scrap: SP4_amont-2025-1
+	aaaaaia => {
+		 F => "data.159",
+		 B => "data.160",
+		 I => "data.160bg",
+		 C => "data.160clip",
+		 E => "data.161",
+		 X => "data.162",
+		 P => "data.162bbox",
+		 Y => 1,
+		 V => -35,
+		 S => "7357.52 -2803.47",
+	},
+	# scrap: SP4_amont-2025-1
+	aaaaaib => {
+		 B => "data.160",
+		 I => "data.160bg",
+		 C => "data.160clip",
+		 Y => 14,
+		 V => -1,
+		 S => "7357.52 -2803.47",
+	},
+	# scrap: SP2_aval-2025-2
+	aaaaaic => {
+		 F => "data.163",
+		 B => "data.164",
+		 I => "data.164bg",
+		 C => "data.164clip",
+		 E => "data.165",
+		 X => "data.166",
+		 P => "data.166bbox",
+		 Y => 1,
+		 V => -41,
+		 S => "7792.05 -1076.87",
+	},
+	# scrap: SP2_aval-2025-2
+	aaaaaid => {
+		 B => "data.164",
+		 I => "data.164bg",
+		 C => "data.164clip",
+		 Y => 15,
+		 V => -1,
+		 S => "7792.05 -1076.87",
+	},
+	# scrap: SP1_aval-2025-2
+	aaaaaie => {
+		 F => "data.167",
+		 B => "data.168",
+		 I => "data.168bg",
+		 C => "data.168clip",
+		 E => "data.169",
+		 X => "data.170",
+		 P => "data.170bbox",
+		 Y => 1,
+		 V => -40,
+		 S => "7947.68 -1870.19",
+	},
+	# scrap: SP1_aval-2025-2
+	aaaaaif => {
+		 B => "data.168",
+		 I => "data.168bg",
+		 C => "data.168clip",
+		 Y => 15,
+		 V => -1,
+		 S => "7947.68 -1870.19",
+	},
+	# scrap: SP1_aval-2025-1
+	aaaaaig => {
+		 F => "data.171",
+		 B => "data.172",
+		 I => "data.172bg",
+		 C => "data.172clip",
+		 E => "data.173",
+		 X => "data.174",
+		 P => "data.174bbox",
+		 Y => 1,
+		 V => -42,
+		 S => "7517.96 -2119.50",
+	},
+	# scrap: SP1_aval-2025-1
+	aaaaaih => {
+		 B => "data.172",
+		 I => "data.172bg",
+		 C => "data.172clip",
+		 Y => 16,
+		 V => -1,
+		 S => "7517.96 -2119.50",
+	},
+);
+
+
+%LAYER = (
+	# expanded map: MP_murs-et-centerline
+	1 => {
+		U => "6",
+		N => 'MP\_murs-et-centerline',
+		T => 'MP\_murs-et-centerline',
+	},
+	# basic map: MP_escalier
+	2 => {
+		Z => 1,
+		A => 1,
+	},
+	# basic map: MP_pieuvre_PG-FM
+	3 => {
+		Z => 1,
+		A => 1,
+	},
+	# basic map: MP_2023
+	4 => {
+		Z => 1,
+		A => 1,
+	},
+	# basic map: MP_tube-de-prechauffage
+	5 => {
+		Z => 1,
+		A => 1,
+	},
+	# basic map: MP_tube-de-prechauffage
+	6 => {
+		Z => 1,
+		A => 1,
+	},
+	# basic map: MP_pts_oublie
+	7 => {
+		Z => 1,
+		A => 1,
+	},
+	# basic map: MP_diaclase_MK-SP
+	8 => {
+		Z => 1,
+		A => 1,
+	},
+	# basic map: MP_amont24
+	9 => {
+		Z => 1,
+		A => 1,
+	},
+	# basic map: MP_pts-actif
+	10 => {
+		Z => 1,
+		A => 1,
+	},
+	# basic map: MP_diaclase-boucle
+	11 => {
+		Z => 1,
+		A => 1,
+	},
+	# basic map: MP_faille-fossile-riviere
+	12 => {
+		Z => 1,
+		A => 1,
+	},
+	# basic map: MP_amont-2025-2
+	13 => {
+		Z => 1,
+		A => 1,
+	},
+	# basic map: MP_amont-2025-1
+	14 => {
+		Z => 1,
+		A => 1,
+	},
+	# basic map: MP_aval-2025-2
+	15 => {
+		Z => 1,
+		A => 1,
+	},
+	# basic map: MP_aval-2025-1
+	16 => {
+		Z => 1,
+		A => 1,
+	},
+	# basic map: 
+	17 => {
+		Z => 1,
+		A => 1,
+	},
+);
+
+
+$MAP_PREVIEW{U} = "6";
+
+
+$PATTERN_DEFS = "./";
